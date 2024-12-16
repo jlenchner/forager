e52ec919d4377d5fedb008ae19421fe5687dba60
jlenchner <jon.lenchner@gmail.com>
2024-12-16 12:25:56 -0500

    First seemingly working version

--- randomGraph.py
+++ randomGraph.py
@@ -3,14 +3,6 @@ import matplotlib.pyplot as plt
 import random
 from smartNode import SmartNode
 
-def GetNextPowerOfTwo(k):
-    n = 1
-    while True:
-        if k <= n:
-            return n
-        else:
-            n *= 2
-
 def GetNeighbors(G, node):
     return set(G.adj._atlas.get(node).keys())
 
@@ -23,9 +15,12 @@ def PrintAllNodeInfo(G):
 n = 20 #number of nodes
 SmartNode.SetNumberOfNodes(numNodes=n)
 p = 0.25 #E-R edge prob
+R = 10 #Number of w-swappiong rounds
+xR = 10 #Number of E-swapping rounds
 
 # Generate a random graph (Erdős-Rényi model)
-G = nx.gnp_random_graph(n, 0.25)  # 10 nodes, probability of edge creation = 0.3
+G = nx.gnp_random_graph(n, 0.25)
+SmartNode.SetGraph(graph=G)
 PROB_HAS_FOOD = 0.5
 # Draw the graph
 color_map = []
@@ -38,7 +33,7 @@ for node in G.nodes:
     else:
         color_map.append('red')
     neighbors = GetNeighbors(G, node)
-    smartNode = SmartNode(node, hasFood=hasFood, neighborSet=neighbors)
+    smartNode = SmartNode(id=node, hasFood=hasFood, neighborSet=neighbors)
     G.nodes._nodes.update({node: smartNode})
 
 PrintAllNodeInfo(G)
@@ -47,5 +42,37 @@ pos = nx.spring_layout(G) #Could be used to display w vectors or perhaps the com
 plt.figure(num='Random Forager Graph')
 nx.draw(G, pos = pos, node_color=color_map, with_labels=True)
 plt.show()
+
+#Complete message passing rounds
+for i in range(R):
+    for node in G.nodes:
+        smartNode = G.nodes._nodes.get(node)
+        smartNode.pullWVecsFromNeighbors()
+    for node in G.nodes:
+        smartNode = G.nodes._nodes.get(node)
+        smartNode.mergeWVecs()
+
+    print("\nw() Swapping Round " + str(i+1) + " of " + str(R) + ':')
+    PrintAllNodeInfo(G)
+
+#Now get everybody to compute their Es
+for node in G.nodes:
+    smartNode = G.nodes._nodes.get(node)
+    smartNode.computeE()
+
+#Complete E-swapping rounds
+for i in range(xR):
+    for node in G.nodes:
+        smartNode = G.nodes._nodes.get(node)
+        smartNode.pullEFromNeighbors()
+    for node in G.nodes:
+        smartNode = G.nodes._nodes.get(node)
+        smartNode.updateE()
+
+    print("\nE Swapping Round " + str(i + 1) + " of " + str(xR) + ':')
+    PrintAllNodeInfo(G)
+
+
+
 
 
